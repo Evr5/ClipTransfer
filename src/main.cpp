8929f5c82030d26b70af96a529079ecf2c1d4513
Ethan Van Ruyskensvelde <ethanvr3@gmail.com>
2025-06-22 13:10:41 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/**
+* @file main.cpp
+* @brief Entry point for the main program.
+* @author Evr5
+* @date 2025
+*
+* This file contains the main function of the C++ Starter Template project.
+*/
+
+#include <fmt/core.h>
+
+int main() {
+    fmt::print("Hello from C++ cross-platform template using fmt!\n");
+    return 0;
+}
